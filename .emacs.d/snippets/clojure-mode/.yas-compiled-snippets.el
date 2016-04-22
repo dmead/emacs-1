658123dc7e34e66b4974112cbd308bf3d21fe65e
Paul Santa Clara <psantaclara@rjmetrics.com>
2016-04-22 09:11:53 -0400

    stuff

--- .emacs.d/snippets/clojure-mode/.yas-compiled-snippets.el
+++ .emacs.d/snippets/clojure-mode/.yas-compiled-snippets.el
@@ -2,7 +2,8 @@
 ;;; Snippet definitions:
 ;;;
 (yas-define-snippets 'clojure-mode
-                     '(("jhost" "\n(.getHostName (java.net.InetAddress/getLocalHost)) $0\n" "jhost" nil nil nil "/Users/psantaclara/.emacs.d/snippets/clojure-mode/jhost" nil nil)))
+                     '(("jhost" "\n(.getHostName (java.net.InetAddress/getLocalHost)) $0\n" "jhost" nil nil nil "/home/rjmetrics/.emacs.d/snippets/clojure-mode/jhost" nil nil)
+                       ("com" "\n(comment\n  $0\n)\n" "com" nil nil nil "/home/rjmetrics/.emacs.d/snippets/clojure-mode/comment" nil nil)))
 
 
-;;; Do not edit! File generated at Mon Feb 22 20:23:44 2016
+;;; Do not edit! File generated at Thu Mar  3 09:12:37 2016
